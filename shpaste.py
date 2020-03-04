423b4e74b70c730b320246b5a0804bcca88120d8
waldemar <hellodoge@tuta.io>
2020-03-04 16:29:48 +0300

    refactor shpaste.py (links logic updated)

--- shpaste.py
+++ shpaste.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, abort
 from flask_sqlalchemy import SQLAlchemy
 from config import Configuration
 from random import getrandbits
@@ -12,14 +12,19 @@ db = SQLAlchemy(app)
 
 class Entity(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    public = db.Column(db.Integer, unique=True, nullable=False)
-    private = db.Column(db.Integer, unique=True, nullable=False)
+    public = db.Column(db.Integer, nullable=False)
+    private = db.Column(db.Integer, nullable=False)
     text = db.Column(db.Text)
 
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'Entity': Entity}
+
+
+def wrap(entity_id, link):
+    link = hex(link)[2:]
+    return hex(entity_id)[2:] + '0'*(8-len(link)) + link
 
 
 @app.route('/')
@@ -30,41 +35,41 @@ def index():
 
 @app.route('/create')
 def create():
-    while True:
-        public = getrandbits(31)
-        private = getrandbits(31)
-        public_check = Entity.query.filter_by(public=public).first()
-        private_check = Entity.query.filter_by(public=public).first()
-        if public_check or private_check:
-            continue
-        break
+    public = getrandbits(31)
+    private = getrandbits(31)
     entity = Entity(public=public, private=private)
     db.session.add(entity)
     db.session.commit()
-    return f'Public link {Configuration.SITE_URL}/{hex(public)[2:]}\n' + \
-           f'Private link {Configuration.SITE_URL}/{hex(private)[2:]}\n'
+    return f'Public link {Configuration.SITE_URL}/{wrap(entity.id, public)}\n' + \
+           f'Private link {Configuration.SITE_URL}/{wrap(entity.id, private)}\n'
 
 
 @app.route('/<entity_public>')
 def view(entity_public):
     try:
-        entity_public = int(entity_public, 16)
+        entity_id = int(entity_public[:-8], 16)
+        public = int(entity_public[-8:], 16)
     except ValueError:
-        return 'Public link is invalid\n'
-    entity = Entity.query.filter_by(public=entity_public).first_or_404()
+        abort(404)
+    entity = Entity.query.get_or_404(entity_id)
+    if public != entity.public:
+        abort(404)
     return str(entity.text)
 
 
 @app.route('/<entity_private>/<text>')
 def change(entity_private, text):
     try:
-        entity_private = int(entity_private, 16)
+        entity_id = int(entity_private[:-8], 16)
+        private = int(entity_private[-8:], 16)
     except ValueError:
-        return 'Private link is invalid\n'
-    entity = Entity.query.filter_by(private=entity_private).first_or_404()
+        abort(404)
+    entity = Entity.query.get_or_404(entity_id)
+    if private != entity.private:
+        abort(404)
     entity.text = text
     db.session.commit()
-    return f'Public link {Configuration.SITE_URL}/{hex(entity.public)[2:]}\n'
+    return f'Public link {Configuration.SITE_URL}/{wrap(entity.id, entity.public)}\n'
 
 
 if __name__ == '__main__':
